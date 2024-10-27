a19339e4f47635f054997b6d48134ac1eff6e325
Anton Chukin <anton.chukin5@gmail.com>
2024-10-27 22:15:30 +0300

    commets added

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
-    public double calculate(int creditSum, int years, double yearlyInterestRate) {
+    public double calculate(int creditAmount, int years, double yearlyInterestRate) {
         double monthlyInterestRate = yearlyInterestRate/12/100;
-        double result = creditSum*(monthlyInterestRate/(1-Math.pow(1+monthlyInterestRate,-years*12)));
+        double result = creditAmount*(monthlyInterestRate/(1-Math.pow(1+monthlyInterestRate,-years*12)));
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
+        // new object of class CreditPaymentService
         CreditPaymentService monthlyPayment = new CreditPaymentService();
+
+        // specifying variables
         double yearlyInterestRate = 9.99;
-        int creditSum = 1_000_000;
+        int creditAmount = 1_000_000;
         int years = 1;
-        double payment = monthlyPayment.calculate(creditSum, years, yearlyInterestRate);
+
+        // calling a method and printing the calculation result
+        double payment = monthlyPayment.calculate(creditAmount, years, yearlyInterestRate);
         System.out.println();
         System.out.println("Calculated monthly payment: " + (int) payment + " roubles");
     }
