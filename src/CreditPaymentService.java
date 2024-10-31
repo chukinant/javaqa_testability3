12741b4f4e6cbe1888170c9ba009c60a59f7544e
Anton Chukin <anton.chukin5@gmail.com>
2024-10-31 12:36:46 +0300

    code reformatted

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(int creditAmount, int years, double yearlyInterestRate) {
-        double monthlyInterestRate = yearlyInterestRate/12/100;
-        double result = creditAmount*(monthlyInterestRate/(1-Math.pow(1+monthlyInterestRate,-years*12)));
+        double monthlyInterestRate = yearlyInterestRate / 12 / 100;
+        double result = creditAmount * (monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -years * 12)));
         return result;
     }
 }
